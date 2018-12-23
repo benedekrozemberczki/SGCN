d4ff8f65f1d67104710007c1120538773be981f5
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-12-23 11:43:37 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
+from sgcn import SignedGCNTrainer
 from parser import parameter_parser
 from utils import tab_printer, read_graph, score_printer, save_logs
-from sgcn import SignedGCNTrainer
 
 def main():
     """
@@ -9,13 +9,13 @@ def main():
     args = parameter_parser()
     tab_printer(args)
     edges = read_graph(args)
-    trainer = SignedGCNTrainer(args,edges)
+    trainer = SignedGCNTrainer(args, edges)
     trainer.setup_dataset()
     trainer.create_and_train_model()
-    if args.test_size>0:
+    if args.test_size > 0:
         trainer.save_model()
         score_printer(trainer.logs)
         save_logs(args, trainer.logs)
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
